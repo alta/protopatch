db5991da4760d8579e1e1f99e61fb4e3d19bd640
Randy Reddig <randy@alta.software>
2020-06-18 15:33:42 -0700

    tests/enum: nested enum tests

--- tests/enum/enum_test.go
+++ tests/enum/enum_test.go
@@ -10,6 +10,10 @@ func TestBasic(t *testing.T) {
 	tests.ValidateEnum(t, Basic(0), Basic_name, Basic_value)
 }
 
+func TestNested(t *testing.T) {
+	tests.ValidateEnum(t, Outer_Inner(0), Outer_Inner_name, Outer_Inner_value)
+}
+
 func TestRename(t *testing.T) {
 	tests.ValidateEnum(t, NewName(0), NewName_name, NewName_value)
 }
