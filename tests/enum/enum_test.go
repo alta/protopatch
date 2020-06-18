d0c3f6e5de64c578f1bf726f22372cbf1b88aab9
Randy Reddig <randy@alta.software>
2020-06-18 15:21:08 -0700

    tests/enum: rename test

--- tests/enum/enum_test.go
+++ tests/enum/enum_test.go
@@ -9,3 +9,7 @@ import (
 func TestBasic(t *testing.T) {
 	tests.ValidateEnum(t, Basic(0), Basic_name, Basic_value)
 }
+
+func TestRename(t *testing.T) {
+	tests.ValidateEnum(t, NewName(0), NewName_name, NewName_value)
+}
