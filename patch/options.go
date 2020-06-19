8397c5aaa2be69b5a6d1999dfe540a154dcbdd00
Randy Reddig <randy@alta.software>
2020-06-19 10:38:11 -0700

    patch, tests/message: de-nest go.field.options to just go.options
    Based on the assumption that field options are most common, and therefore warrant less typing.

--- patch/options.go
+++ patch/options.go
@@ -3,7 +3,6 @@ package patch
 import (
 	patch_go "github.com/alta/protopatch/patch/go"
 	"github.com/alta/protopatch/patch/go/enum"
-	"github.com/alta/protopatch/patch/go/field"
 	"github.com/alta/protopatch/patch/go/message"
 	"github.com/alta/protopatch/patch/go/oneof"
 	"github.com/alta/protopatch/patch/go/value"
@@ -25,7 +24,7 @@ func messageOptions(m *protogen.Message) *patch_go.Options {
 }
 
 func fieldOptions(f *protogen.Field) *patch_go.Options {
-	return proto.GetExtension(f.Desc.Options(), field.E_Options).(*patch_go.Options)
+	return proto.GetExtension(f.Desc.Options(), patch_go.E_Options).(*patch_go.Options)
 }
 
 func oneofOptions(o *protogen.Oneof) *patch_go.Options {
