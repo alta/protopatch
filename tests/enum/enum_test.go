ebdeb019c46735474cba77541d369c80a248b51c
Connor Hindley <connor.hindley@tanium.com>
2020-06-18 21:57:17 -0600

    tests/enum: add tests for enum stringer.

--- tests/enum/enum_test.go
+++ tests/enum/enum_test.go
@@ -1,6 +1,7 @@
 package enum
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/alta/protopatch/tests"
@@ -16,4 +17,28 @@ func TestNestedEnum(t *testing.T) {
 
 func TestRenamedEnum(t *testing.T) {
 	tests.ValidateEnum(t, RenamedEnum(0), RenamedEnum_name, RenamedEnum_value)
+}
+
+func TestStringerName(t *testing.T) {
+	tests := []struct {
+		enum     CustomStringer
+		original string
+		patched  string
+	}{
+		{0, "INVALID", "invalid"},
+		{1, "A", "a"},
+		{2, "B", "b"},
+		{3, "C", "c"},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("enum(%d)/%s/%s", int32(tt.enum), tt.original, tt.patched)
+		t.Run(name, func(t *testing.T) {
+			if s := tt.enum.OrigString(); s != tt.original {
+				t.Errorf("%T(%d) incorrect original string %q != %q", tt.enum, tt.enum, s, tt.original)
+			}
+			if s := tt.enum.String(); s != tt.patched {
+				t.Errorf("%T(%d) incorrect patched string %q != %q", tt.enum, tt.enum, s, tt.patched)
+			}
+		})
+	}
 }
