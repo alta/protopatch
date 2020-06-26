ee952e91aa0a5aaf96abce27c78ffda4c716dd59
Randy Reddig <randy@alta.software>
2020-06-26 08:39:12 -0700

    patch: add exported Options func to get patch options

--- patch/options.go
+++ patch/options.go
@@ -10,7 +10,17 @@ import (
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
+
+// Options returns patch_go.Options if present on desc, otherwise nil.
+func Options(desc protoreflect.Descriptor, xt protoreflect.ExtensionType) *patch_go.Options {
+	o := desc.Options()
+	if proto.HasExtension(o, xt) {
+		return proto.GetExtension(o, xt).(*patch_go.Options)
+	}
+	return nil
+}
 
 func enumOptions(e *protogen.Enum) *patch_go.Options {
 	return proto.GetExtension(e.Desc.Options(), enum.E_Options).(*patch_go.Options)
@@ -26,11 +36,14 @@ func messageOptions(m *protogen.Message) *patch_go.Options {
 
 func fieldOptions(f *protogen.Field) *patch_go.Options {
 	// First try (go.field.options)
-	if proto.HasExtension(f.Desc.Options(), field.E_Options) {
-		return proto.GetExtension(f.Desc.Options(), field.E_Options).(*patch_go.Options)
+	if opts := Options(f.Desc, field.E_Options); opts != nil {
+		return opts
 	}
 	// Then try (go.field.options)
-	return proto.GetExtension(f.Desc.Options(), patch_go.E_Options).(*patch_go.Options)
+	if opts := Options(f.Desc, patch_go.E_Options); opts != nil {
+		return opts
+	}
+	return nil
 }
 
 func oneofOptions(o *protogen.Oneof) *patch_go.Options {
