c8f1a6116f8646008a1b35c36337d0da425773fa
Randy Reddig <randy@alta.software>
2020-06-27 13:51:00 -0700

    patch: rename Options to GoOptions
    It was moved in the previous commit from patch/go into patch, so this change makes the name more accurately reflect its purpose.

--- patch/options.go
+++ patch/options.go
@@ -5,22 +5,22 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func enumOptions(e *protogen.Enum) *Options {
-	return proto.GetExtension(e.Desc.Options(), E_Enum).(*Options)
+func enumOptions(e *protogen.Enum) *GoOptions {
+	return proto.GetExtension(e.Desc.Options(), E_Enum).(*GoOptions)
 }
 
-func valueOptions(v *protogen.EnumValue) *Options {
-	return proto.GetExtension(v.Desc.Options(), E_Value).(*Options)
+func valueOptions(v *protogen.EnumValue) *GoOptions {
+	return proto.GetExtension(v.Desc.Options(), E_Value).(*GoOptions)
 }
 
-func messageOptions(m *protogen.Message) *Options {
-	return proto.GetExtension(m.Desc.Options(), E_Message).(*Options)
+func messageOptions(m *protogen.Message) *GoOptions {
+	return proto.GetExtension(m.Desc.Options(), E_Message).(*GoOptions)
 }
 
-func fieldOptions(f *protogen.Field) *Options {
-	return proto.GetExtension(f.Desc.Options(), E_Field).(*Options)
+func fieldOptions(f *protogen.Field) *GoOptions {
+	return proto.GetExtension(f.Desc.Options(), E_Field).(*GoOptions)
 }
 
-func oneofOptions(o *protogen.Oneof) *Options {
-	return proto.GetExtension(o.Desc.Options(), E_Oneof).(*Options)
+func oneofOptions(o *protogen.Oneof) *GoOptions {
+	return proto.GetExtension(o.Desc.Options(), E_Oneof).(*GoOptions)
 }
