d5fd56ab854a5fd9f153859904d09f573f183715
Randy Reddig <randy@alta.software>
2020-12-23 15:57:41 -0800

    go.{mod,sum}, internal/tools: add protoc-gen-validate as dependency

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -3,10 +3,12 @@
 package tools
 
 import (
+	_ "github.com/envoyproxy/protoc-gen-validate"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
 
+//go:generate go install github.com/envoyproxy/protoc-gen-validate
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate go install ../../cmd/protoc-gen-go-patch
