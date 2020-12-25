49f87b28a9f401fc46e27230b50ca1d8a6f2bcfe
Randy Reddig <randy@alta.software>
2020-12-25 15:44:08 -0800

    tests/lint: smoke tests for (go.lint) option

---
+++ tests/lint/lint_test.go
@@ -0,0 +1,41 @@
+package lint
+
+import (
+	"testing"
+
+	"github.com/alta/protopatch/tests"
+)
+
+func TestProtocol(t *testing.T) {
+	tests.ValidateEnum(t, Protocol(0), Protocol_name, Protocol_value)
+	tests.ValidateEnum(t, ProtocolInvalid, Protocol_name, Protocol_value)
+	tests.ValidateEnum(t, ProtocolIP, Protocol_name, Protocol_value)
+	tests.ValidateEnum(t, ProtocolUDP, Protocol_name, Protocol_value)
+	tests.ValidateEnum(t, ProtocolTCP, Protocol_name, Protocol_value)
+}
+
+func TestURL(t *testing.T) {
+	tests.ValidateMessage(t, &URL{})
+}
+
+func TestID(t *testing.T) {
+	tests.ValidateMessage(t, &ID{})
+}
+
+func TestOneofMessage(t *testing.T) {
+	m := &OneofMessage{}
+	tests.ValidateMessage(t, m)
+	var _ isOneofMessage_Contents = &OneofMessage_ID{}
+	var _ isOneofMessage_Contents = &OneofMessage_URL{}
+	var _ *ID = m.GetID()
+	var _ *URL = m.GetURL()
+}
+
+func TestOuterMessage(t *testing.T) {
+	m := &OuterMessage{}
+	tests.ValidateMessage(t, &OuterMessage{})
+	tests.ValidateMessage(t, &OuterMessageInnerID{})
+	tests.ValidateMessage(t, &OuterMessageInnerURL{})
+	var _ *OuterMessageInnerID = m.GetID()
+	var _ *OuterMessageInnerURL = m.GetURL()
+}
