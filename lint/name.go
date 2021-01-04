fc5c598f0c5f39f04813412ae9efcaa5412824db
Randy Reddig <randy@alta.software>
2021-01-04 13:02:27 -0800

    lint: add Go authors copyright notice

--- lint/name.go
+++ lint/name.go
@@ -1,6 +1,12 @@
-package lint
-
 // Copied from https://github.com/golang/lint
+//
+// Copyright (c) 2013 The Go Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd.
+
+package lint
 
 import (
 	"strings"
